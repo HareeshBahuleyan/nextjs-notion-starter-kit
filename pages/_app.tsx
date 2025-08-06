22d1f61f26f18e1bb7f462385ff607f32bffa4b6
Hareesh <hareeshbahuleyan@gmail.com>
2025-08-07 00:00:47 +0200

    Add support for google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/next'
 import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { posthog } from 'posthog-js'
 import * as React from 'react'
 
@@ -35,6 +36,8 @@ if (!isServer) {
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
+
+  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
 
   React.useEffect(() => {
     function onRouteChangeComplete() {
@@ -64,6 +67,22 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      {gaMeasurementId && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+            strategy='afterInteractive'
+          />
+          <Script id='google-analytics' strategy='afterInteractive'>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaMeasurementId}');
+            `}
+          </Script>
+        </>
+      )}
       <Component {...pageProps} />
       <Analytics />
     </>
